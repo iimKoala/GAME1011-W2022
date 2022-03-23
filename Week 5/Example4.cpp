f95d0d6dec49ea31d2d96a9d39bd3faf3157ed7d
RJ Haye <rjhaye100@yahoo.com>
2022-03-22 20:21:04 -0400

    Added push_back and pop_back

--- Week 5/Example4.cpp
+++ Week 5/Example4.cpp
@@ -1,55 +1,58 @@
+// This program demonstrates the SimpleVector template.
 #include <iostream>
 #include "SimpleVector.h"
 using namespace std;
 
-//int main()
-//{
-//	const int SIZE = 10;
-//
-//	int i = 0;
-//
-//	SimpleVector<int> intTable(SIZE);
-//	SimpleVector<double> doubleTable(SIZE);
-//
-//	// Store value in the array
-//	for (i = 0; i < SIZE; i++)
-//	{
-//		intTable[i] = (i * 2);
-//		doubleTable[i] = (i * 2.12);
-//	}
-//
-//	// Display the values in the array
-//	cout << "These values are in intTable:\n";
-//	intTable.print();
-//
-//	cout << "These values are in doubleTable:\n";
-//	doubleTable.print();
-//
-//	// Demo the built in + operator
-//	for (i = 0; i < SIZE; i++)
-//	{
-//		intTable[i] += 5;
-//		doubleTable[i] += 5.3;
-//	}
-//
-//	cout << "These values are in intTable:\n";
-//	intTable.print();
-//
-//	cout << "These values are in doubleTable:\n";
-//	doubleTable.print();
-//
-//	// Demo the ++ operator
-//	for (i = 0; i < SIZE; i++)
-//	{
-//		intTable[i]++;
-//		doubleTable[i]++;
-//	}
-//
-//	cout << "These values are in intTable:\n";
-//	intTable.print();
-//
-//	cout << "These values are in doubleTable:\n";
-//	doubleTable.print();
-//
-//	return 0;
-//}
+int main()
+{
+    const int SIZE = 10; // Number of elements
+    int amtToPrnt = 10;
+    int count; // Loop counter
+
+// Create a SimpleVector of ints.
+    SimpleVector<int> intTable(SIZE);
+
+    // Store values in the two SimpleVectors.
+    for (count = 0; count < SIZE; count++)
+    {
+        intTable[count] = (count * 2);
+    }
+
+    // Display the values in the SimpleVectors.
+    cout << "These values are in intTable:\n";
+    for (count = 0; count < amtToPrnt; count++)
+        cout << intTable[count] << " ";
+    cout << endl;
+
+    intTable.push_back(20);
+    amtToPrnt++;
+
+    cout << "These values are in intTable after adding one more value:\n";
+    for (count = 0; count < amtToPrnt; count++)
+        cout << intTable[count] << " ";
+    cout << endl;
+
+
+
+
+
+    cout << "These values are in intTable:\n";
+    for (count = 0; count < amtToPrnt; count++)
+        cout << intTable[count] << " ";
+    cout << endl;
+
+    intTable.pop_back(20);
+    amtToPrnt--;
+
+    cout << "These values are in intTable after removing one more value:\n";
+    for (count = 0; count < amtToPrnt; count++)
+        cout << intTable[count] << " ";
+    cout << endl;
+
+
+
+    
+    
+     
+    return 0;
+}
--- Week 5/SimpleVector.h
+++ Week 5/SimpleVector.h
@@ -1,92 +1,181 @@
-#pragma once
+// SimpleVector class template
+#ifndef SIMPLEVECTOR_H
+#define SIMPLEVECTOR_H
 #include <iostream>
-#include <cstdlib>
+#include <new>       // Needed for bad_alloc exception
+#include <cstdlib>   // Needed for the exit function
+
 using namespace std;
 
-template <class T>
+template<class T>
 class SimpleVector
 {
 private:
-	T* aptr;							// Pointer to dynamically allocated array
-	int arraySize;						// Size of the array
-	void subError() const;				// Handle subscript out of range errors
+    T* aptr;          // To point to the allocated array
+    int arraySize;    // Number of elements in the array
+    void memError();  // Handles memory allocation errors
+    void subError();  // Handles subscripts out of range
+
 public:
-	SimpleVector(int);					// Constructor
-	SimpleVector(const SimpleVector&);	// Copy constructor
-	~SimpleVector();					// Destructor
-	int size() const
-	{
-		return arraySize;
-	}
-	T& operator[](int);					// Overloaded [] operator
-	void print() const;					// Output array elements
+    // Default constructor
+    SimpleVector()
+    {
+        aptr = 0;
+        arraySize = 0;
+    }
+
+    // Constructor declaration
+    SimpleVector(int);
+
+    // Copy constructor declaration
+    SimpleVector(const SimpleVector&);
+
+    // Destructor declaration
+    ~SimpleVector();
+
+    // Accessor to return the array size
+    int size() const
+    {
+        return arraySize;
+    }
+
+    // Accessor to return a specific element
+    T getElementAt(int position);
+
+    // Overloaded [] operator declaration
+    T& operator[](const int&);
+
+    void push_back(int);
+
+    void pop_back(int);
 };
 
-// Constructors
-template <class T>
-SimpleVector<T>::SimpleVector(int arraySize)
+
+
+template<class T>
+SimpleVector<T>::SimpleVector(int s)
 {
-	this->arraySize = arraySize;
-	aptr = new T[arraySize];
-	for (int i = 0; i < arraySize; i++)
-	{
-		aptr[i] = int();
-	}
+    arraySize = s;
+    // Allocate memory for the array.
+    try
+    {
+        aptr = new T[s];
+    }
+    catch (bad_alloc)
+    {
+        memError();
+    }
+
+    // Initialize the array.
+    for (int count = 0; count < arraySize; count++)
+        *(aptr + count) = 0;
 }
 
-// Copy constructor
-template <class T>
+
+template<class T>
 SimpleVector<T>::SimpleVector(const SimpleVector& obj)
 {
-	arraySize = obj.arraySize;
-	aptr = new T[arraySize];
-	for (int i = 0; i < arraySize; i++)
-	{
-		aptr[i] = obj.aptr[i];
-	}
+    // Copy the array size.
+    arraySize = obj.arraySize;
+
+    // Allocate memory for the array.
+    aptr = new T[arraySize];
+    if (aptr == 0)
+        memError();
+
+    // Copy the elements of obj's array.
+    for (int count = 0; count < arraySize; count++)
+        *(aptr + count) = *(obj.aptr + count);
 }
 
-// Destructor
-template <class T>
+
+
+
+template<class T>
 SimpleVector<T>::~SimpleVector()
 {
-	if (arraySize > 0)
-	{
-		delete[] aptr;
-	}
-
-	aptr = nullptr;
+    if (arraySize > 0)
+        delete[] aptr;
 }
 
-// SubError function
-template <class T>
-void SimpleVector<T>::subError() const
+
+
+template<class T>
+void SimpleVector<T>::memError()
 {
-	cout << "ERROR: Subscript out of range.\n";
-	exit(0);
+    cout << "ERROR:Cannot allocate memory.\n";
+    exit(EXIT_FAILURE);
 }
 
-// Overload the [] operator.
-template <class T>
-T& SimpleVector<T>::operator[](int sub)
-{
-	if (sub < 0 || sub >= arraySize)
-	{
-		// Sub value out of range
-		subError();
-	}
 
-	// Sub value is fine
-	return aptr[sub];
+
+
+template<class T>
+void SimpleVector<T>::subError()
+{
+    cout << "ERROR: Subscript out of range.\n";
+    exit(EXIT_FAILURE);
 }
 
-// Print all entries in the array
-template <class T>
-void SimpleVector<T>::print() const
+
+template<class T>
+T SimpleVector<T>::getElementAt(int sub)
 {
-	for (int i = 0; i < arraySize; i++)
-	{
-		cout << aptr[i] << " ";
-	}
-	cout << endl;
+    if (sub < 0 || sub >= arraySize)
+        subError();
+    return aptr[sub];
+}
+
+
+
+template<class T>
+T& SimpleVector<T>::operator[](const int& sub)
+{
+    if (sub < 0 || sub >= arraySize)
+        subError();
+    return aptr[sub];
+}
+#endif
+
+template <class T>
+void SimpleVector<T>::push_back(int newval) {
+    // Allocate memory for the array in the temporary array.
+    T* tmpArray = new T[arraySize + 1];
+
+    // Copy the elements of old array.
+    for (int count = 0; count < arraySize; count++)
+        *(tmpArray + count) = *(aptr + count);
+
+    // Push new value
+    *(tmpArray + arraySize) = newval;
+
+    // Delete old array
+    delete[] aptr;
+
+    // Copy array
+    aptr = tmpArray;
+
+    // Increase size
+    arraySize++;
+}
+template <class T>
+void SimpleVector<T>::pop_back(int newval) {
+    // Allocate memory for the array in the temporary array.
+    T* tmpArray = new T[arraySize + 1];
+
+    // Copy the elements of old array.
+    for (int count = 0; count < arraySize; count++)
+        *(tmpArray + count) = *(aptr + count);
+
+    // Push new value
+    *(tmpArray - arraySize) = newval;
+
+    // Delete old array
+    delete[] aptr;
+
+    // Copy array
+    aptr = tmpArray;
+
+    // Increase size
+    arraySize--;
 }
